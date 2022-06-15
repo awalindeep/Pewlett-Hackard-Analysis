84ea0633b798ae467220406d95febfbc4543c3e9
awalindeep <awalindeep@live.com>
2022-06-14 21:02:30 -0400

    Commit

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/queries.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/queries.sql
@@ -121,3 +121,69 @@ ORDER BY de.dept_no;
 -- Check the table
 SELECT * FROM retirement_count;
 
+-- Join employees, salaries, and dept_emp tables to create new table 'emp_info'
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+    AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+
+ -- List of managers per department
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+INNER JOIN departments AS d
+	ON (dm.dept_no = d.dept_no)
+INNER JOIN current_emp AS ce
+	ON (dm.emp_no = ce.emp_no);
+
+-- List of retirees per department
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- List of retirees per sales department
+SELECT r.emp_no,r.first_name, r.last_name, d.dept_name
+INTO sales_dept_info
+FROM retirement_info as r
+INNER JOIN dept_emp AS de
+ON (r.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = 'Sales';
+
+-- List of retirees per sales and development department
+SELECT r.emp_no,r.first_name, r.last_name, d.dept_name
+INTO sales_development_dept_info
+FROM retirement_info as r
+INNER JOIN dept_emp AS de
+ON (r.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
+
+-- Check the table
+SELECT * FROM retirement_count;
